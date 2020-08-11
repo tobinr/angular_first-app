0ff47aa352c93e282784519a0974080c712f48de
Tobin Risser <tobinrisser@gmail.com>
2020-08-11 15:14:53 -0700

    added server component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+//Class Decorator
+@Component({
+    //metadata for the class
+    //the HTML tag to be used for this element
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+// Standard TypeScript class
+export class ServerComponent {
+
+}
